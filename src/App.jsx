5753687d169c3c81d4ddb53b4a133e7a945a508b
jozhua <joshuazabala693@gmail.com>
2025-05-26 22:16:49 +0800

    added routes for login page and sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Landing Page/LandingPage";
 import LoginPage from "./LoginPage/LoginPage";
+import SignUpPage from "./SignUpPage/SignUpPage";
 
 export default function App () {
     return (
@@ -8,6 +9,7 @@ export default function App () {
             <Routes>
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/LoginPage/LoginPage' element={<LoginPage />} />
+                <Route path='/SignUpPage/SignUpPage' element={<SignUpPage />} />
             </Routes>
         </BrowserRouter>
     );
