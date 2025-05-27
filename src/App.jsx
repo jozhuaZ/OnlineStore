1105ab7ed52782f95c37b01011c7ca66f4376687
jozhua <joshuazabala693@gmail.com>
2025-05-27 15:45:25 +0800

    added Store Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Landing Page/LandingPage";
 import LoginPage from "./LoginPage/LoginPage";
 import SignUpPage from "./SignUpPage/SignUpPage";
+import StorePage from './StorePage/StorePage';
 
 export default function App () {
     return (
@@ -10,6 +11,7 @@ export default function App () {
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/LoginPage/LoginPage' element={<LoginPage />} />
                 <Route path='/SignUpPage/SignUpPage' element={<SignUpPage />} />
+                <Route path='/StorePage/StorePage' element={<StorePage />} />
             </Routes>
         </BrowserRouter>
     );
