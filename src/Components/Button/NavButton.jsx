f0a36852614f55a4f9cf220e5180f57572234f93
jozhua <joshuazabala693@gmail.com>
2025-05-27 15:42:23 +0800

    added ClassicButton

--- src/Components/Button/NavButton.jsx
+++ src/Components/Button/NavButton.jsx
@@ -1,19 +1,24 @@
 
-export const NavButton = ({ title, color, ...rest }) => {
-    let colorValue = '';
-    if (color === 'teal-to-lime') {
-        colorValue = 'text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
-    } else {
-        colorValue = 'text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2';
-    }
+export const NavButton = ({ title, className = '', ...rest }) => {
 
     return (
         // NAVIGATE TO ANOTHER PAGE
         <button
-            className={`${colorValue} cursor-pointer shadow-md/35 hover:scale-[1.03]`}
+            className={`${className} bg-linear-to-br px-5 py-2.5 cursor-pointer shadow-md/35 hover:scale-[1.03] duration-300 ease-in-out rounded-lg text-sm font-medium text-center me-2 mb-2`}
             {...rest}
         >
             <p className='font-poppins text-[18px]'>{title}</p>        
+        </button>
+    );
+};
+
+export const ClassicButton = ({ className = '', text, ...rest }) => {
+    return (
+        <button
+            className={`${className} cursor-pointer duration-300 ease-in-out`}
+            {...rest}
+        >
+            <p className='font-poppins text-[16px]'>{text}</p>    
         </button>
     );
 };
