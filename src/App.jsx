246ffe82c8860f05263646e08445e8338077b660
jozhua <joshuazabala693@gmail.com>
2025-06-21 21:56:45 +0800

    changes to Folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./Landing Page/LandingPage";
-import LoginPage from "./LoginPage/LoginPage";
-import SignUpPage from "./SignUpPage/SignUpPage";
-import StorePage from './StorePage/Storepage';
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import StorePage from './pages/StorePage';
 
 export default function App () {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<LandingPage />} />
-                <Route path='/LoginPage/LoginPage' element={<LoginPage />} />
-                <Route path='/SignUpPage/SignUpPage' element={<SignUpPage />} />
-                <Route path='/StorePage/StorePage' element={<StorePage />} />
+                <Route path='/pages/LoginPage' element={<LoginPage />} />
+                <Route path='/pages/SignUpPage' element={<SignUpPage />} />
+                <Route path='/pages/StorePage' element={<StorePage />} />
             </Routes>
         </BrowserRouter>
     );
